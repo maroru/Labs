da8fb9e0dd8a978aae74226c945b86295a188ba2
maroru <matr988@gmail.com>
2020-02-05 13:50:24 -0600

    doWhile loops program

--- LAB1/maxarray.cpp
+++ LAB1/maxarray.cpp
@@ -12,23 +12,30 @@ int maxArray(int anArray[], int first, int last)
 
 int main()
 {
-    int x;
-    std::cout << "Create an array\n" << "Size of array: ";;
-    std::cin >> x;
-    int array[x];
-    for (int i{0} ; i < x ; i++)
+    char repeat {};
+    do 
     {
-        std::cout << "Array[" << i << "] is: ";
-        std::cin >> array[i];
-    }
-    //int n {sizeof(array)/sizeof(array[0])};
-    std::cout << "User-defined array contents: ";
-    for (int i{ 0 }; i < x; i++)
-        std::cout << array[i] << " ";
+        // User defines their own array using cin
+        int size; 
+        std::cout << "Create an array\n" << "Size of array: ";
+        std::cin >> size;
+        int array[size];
+        for (int i{0} ; i < size ; i++)                 // Populate the array
+        {
+            std::cout << "Array[" << i << "] is: ";
+            std::cin >> array[i];
+        }
 
-    std::cout << "\nFinding largest number using multipath recursion...";
-    std::cout << "\nLargest number is: " << maxArray(array, 0, x) << '\n';
-    if (x == 1)
-        main();
+
+        std::cout << "User-defined array contents: ";
+        for (int i{ 0 }; i < size; i++)
+            std::cout << array[i] << " ";
+
+        std::cout << "\nFinding largest number using multipath recursion...";
+        std::cout << "\nLargest number is: " << maxArray(array, 0, size) << '\n';
+
+        std::cout << "Repeat? Y/N\n";
+        std::cin >> repeat;
+    } while ( repeat == 'y' || repeat == 'Y');
     return 0;
 }
