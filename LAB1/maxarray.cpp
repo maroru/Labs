2400a554dc74218aa2ee982b20b6be5fecfe910c
maroru <matr988@gmail.com>
2020-02-05 13:15:54 -0600

    user-defined arrays

--- LAB1/maxarray.cpp
+++ LAB1/maxarray.cpp
@@ -12,12 +12,23 @@ int maxArray(int anArray[], int first, int last)
 
 int main()
 {
-    int array[]{ 1, 2, 3, 4, 5, 6, 7 };
-    int n {sizeof(array)/sizeof(array[0])};
+    int x;
+    std::cout << "Create an array\n" << "Size of array: ";;
+    std::cin >> x;
+    int array[x];
+    for (int i{0} ; i < x ; i++)
+    {
+        std::cout << "Array[" << i << "] is: ";
+        std::cin >> array[i];
+    }
+    //int n {sizeof(array)/sizeof(array[0])};
+    std::cout << "User-defined array contents: ";
+    for (int i{ 0 }; i < x; i++)
+        std::cout << array[i] << " ";
 
-    for (int i{ 0 }; i < n; i++)
-        std::cout << array[i];
-
-    std::cout << "\nLargest number is: " << maxArray(array, 0, n) << '\n';
+    std::cout << "\nFinding largest number using multipath recursion...";
+    std::cout << "\nLargest number is: " << maxArray(array, 0, x) << '\n';
+    if (x == 1)
+        main();
     return 0;
 }
