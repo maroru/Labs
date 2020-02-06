907ca46201044c43b481077dff32a00d05be549d
Matthew Ruiz <matr988@gmail.com>
2020-02-05 22:58:32 -0600

    added comments

--- LAB1/maxarray.cpp
+++ LAB1/maxarray.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+// Template function to find max value of an array
 template <class T>
 T maxArray(T anArray[], int first, int last)
 {
@@ -12,38 +13,46 @@ T maxArray(T anArray[], int first, int last)
     return std::max(maxArray(anArray,first,mid),maxArray(anArray,mid,last));
 }
 
+// Function Declaration
 template <class T>
-void displayArray(T anArray[], int size)
-{
-    int i {0};
-    while ( i < size )
-    {
-        std::cout << anArray[i] << " ";
-        i++;
-    }
-}
+void displayArray(T anArray[], int size);
 
 int main()
 {
     int intArray[7]{ 11, 2, 3, 4, 9, 10, 13};
     char charArray[6]{ 'v', 'b', 'z', 'd', 'e', 'j'};
 
+    std::cout << "Displaying intArray of size 7\n";
     displayArray<int>(intArray, 7);
     std::cout << "\nFinding largest number using multipath recursion...";
     std::cout << "\nLargest number is: "
-              << maxArray<int>(intArray, 0, 7) << '\n';
+              << maxArray<int>(intArray, 0, 7) << "\n\n";
 
+    std::cout << "Displaying charArray of size 6\n";
     displayArray<char>(charArray, 6);
     std::cout << "\nFinding largest character using multipath recursion...";
     std::cout << "\nLargest char is: "
-              << maxArray<char>(charArray, 0, 6) << '\n';
+              << maxArray<char>(charArray, 0, 6) << "\n\n";
     return 0;
 }
 
+// Displays contents of array
+template <class T>
+void displayArray(T anArray[], int size)
+{
+    int i {0};
+    std::cout << "Contents of array: ";
+    while ( i < size )
+    {
+        std::cout << anArray[i];
+        i++;
+        if ( i != size ) { std::cout << ", "; }
+    }
+}
 /* Tested Arrays:
- * Array 1 :<1 , 2 , 9 , 5 , 6 >
- * Result: Largest number is: 9
- *
- * Array 2 :<10 , 4 , 5 , 13>
+ * integer Array : { 11 , 2 , 3 , 4 , 9 , 10 , 13 }
  * Result: Largest number is: 13
+ *
+ * Array 2 : { v , b , z , d , e , j }
+ * Result: Largest character is: z
  */
