6723acf1a8be2f3a00c6d2019d9a96031eb0a9b1
Matthew Ruiz <matr988@gmail.com>
2020-02-05 21:24:54 -0600

    template function support, added display function

--- LAB1/maxarray.cpp
+++ LAB1/maxarray.cpp
@@ -2,40 +2,48 @@
 
 #include <iostream>
 
-int maxArray(int anArray[], int first, int last)
+template <class T>
+T maxArray(T anArray[], int first, int last)
 {
+    
     if ( last-first == 1 )
         return anArray[first];
     int mid { first + (last-first) / 2 };
     return std::max(maxArray(anArray,first,mid),maxArray(anArray,mid,last));
 }
 
+template <class T>
+void displayArray(T anArray[], int size)
+{
+    int i {0};
+    while ( i < size )
+    {
+        std::cout << anArray[i] << " ";
+        i++;
+    }
+}
+
 int main()
 {
-    char repeat {};
-    do 
-    {
-        // User defines their own array using cin
-        int size; 
-        std::cout << "Create an array\n" << "Size of array: ";
-        std::cin >> size;
-        int array[size];
-        for (int i{0} ; i < size ; i++)                 // Populate the array
-        {
-            std::cout << "Array[" << i << "] is: ";
-            std::cin >> array[i];
-        }
+    int intArray[7]{ 11, 2, 3, 4, 9, 10, 13};
+    char charArray[6]{ 'v', 'b', 'z', 'd', 'e', 'j'};
 
+    displayArray<int>(intArray, 7);
+    std::cout << "\nFinding largest number using multipath recursion...";
+    std::cout << "\nLargest number is: "
+              << maxArray<int>(intArray, 0, 7) << '\n';
 
-        std::cout << "User-defined array contents: ";
-        for (int i{ 0 }; i < size; i++)
-            std::cout << array[i] << " ";
-
-        std::cout << "\nFinding largest number using multipath recursion...";
-        std::cout << "\nLargest number is: " << maxArray(array, 0, size) << '\n';
-
-        std::cout << "Repeat? Y/N\n";
-        std::cin >> repeat;
-    } while ( repeat == 'y' || repeat == 'Y');
+    displayArray<char>(charArray, 6);
+    std::cout << "\nFinding largest character using multipath recursion...";
+    std::cout << "\nLargest char is: "
+              << maxArray<char>(charArray, 0, 6) << '\n';
     return 0;
 }
+
+/* Tested Arrays:
+ * Array 1 :<1 , 2 , 9 , 5 , 6 >
+ * Result: Largest number is: 9
+ *
+ * Array 2 :<10 , 4 , 5 , 13>
+ * Result: Largest number is: 13
+ */
