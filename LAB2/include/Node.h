c9f8fefbb6fa850069875631041b9aae0b17a7d1
maroru <matr988@gmail.com>
2020-02-24 12:00:33 -0600

    removed unneccesary headers & comments

--- LAB2/include/Node.h
+++ LAB2/include/Node.h
@@ -11,24 +11,3 @@ class Node {
         Node* getNext();
         void setNext(Node* anode);
 };
-/*
-char Node::getItem()
-{
-    return item;
-}
-
-void Node::setItem(char aitem)
-{
-    item = aitem;
-}
-
-Node * Node::getNext()
-{
-    return next;
-}
-
-void Node::setNext(Node* anode)
-{
-    next = anode;
-}
-*/
