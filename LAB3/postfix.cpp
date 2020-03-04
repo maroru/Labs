4da890edc0b6795c7812b6f9076153cc4b8d457b
maroru <matr988@gmail.com>
2020-03-04 12:19:45 -0600

    foundation

--- LAB3/postfix.cpp
+++ LAB3/postfix.cpp
@@ -1,12 +1,33 @@
 // Matthew Ruiz
 #include <iostream>
 
-int postfixCalculator(int stringArr[])
+int postfixCalculator(int stringArr[], int max)
 {
+    for (int i = 0; i < max ; i++)
+    {
+        if (stringArr[i] != ( '*' || '/' || '+' || '-' ))
+            push(stringArr[i]);
+        else // ch is an operator named op
+        {
+            // Evaluate and push the result
+            operand2 = top;
+            pop;
+
+            operand1 = top;
+            pop;
+
+            result = operand1 op operand2;
+        }
+    }
     return result;
 }
 
 int main()
 {
+    std::string newString[];
+    std::cout << "Enter: ";
+    std::getline(std::cin, newString[]);
+    std::cout << '\n';
+    std::cout << "Result: " << postfixCalculator(newString[], max);
     return 0;
 }
